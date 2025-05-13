1805ebeca442a97cec0f3f2ba9416b3643395030
Илья Акимов <akimov.ilya2018@mail.com>
2025-05-13 12:08:06 +0300

    Update test_symbol_table.cpp

--- pascal_minus_minus_ide/tests/test_symbol_table.cpp
+++ pascal_minus_minus_ide/tests/test_symbol_table.cpp
@@ -1,27 +1,63 @@
 #include <gtest/gtest.h>
-#include "postfix.h"
+#include "symbol_table.h"
 
-TEST(PostfixTest, ConvertSimpleExpression) {
-    PostfixConverter converter;
-    EXPECT_EQ(converter.toPostfix("2 + 3"), "2 3 +");
+// Тест: Вставка и последующий поиск символа
+TEST(SymbolTableTest, InsertAndLookup) {
+    SymbolTable table;
+
+    // Вставляем переменную "a" с типом Variable и значением 1
+    EXPECT_TRUE(table.insert("a", SymbolType::Variable, 1));
+
+    SymbolInfo info;
+
+    // Проверяем, что символ найден
+    EXPECT_TRUE(table.lookup("a", info));
+
+    // Проверяем, что значение корректно
+    EXPECT_EQ(info.value, 1);
 }
 
-TEST(PostfixTest, ConvertComplexExpression) {
-    PostfixConverter converter;
-    EXPECT_EQ(converter.toPostfix("2 + 3 * 4"), "2 3 4 * +");
+// Тест: Попытка повторной вставки того же идентификатора
+TEST(SymbolTableTest, InsertDuplicate) {
+    SymbolTable table;
+
+    // Первая вставка должна пройти успешно
+    EXPECT_TRUE(table.insert("a", SymbolType::Variable, 1));
+
+    // Повторная вставка с тем же именем должна быть отклонена
+    EXPECT_FALSE(table.insert("a", SymbolType::Variable, 2));
 }
 
-TEST(PostfixTest, EvaluateSimple) {
-    PostfixEvaluator evaluator;
-    EXPECT_EQ(evaluator.evaluate("2 3 +"), 5);
+// Тест: Поиск несуществующего идентификатора
+TEST(SymbolTableTest, LookupNotExist) {
+    SymbolTable table;
+    SymbolInfo info;
+
+    // Ожидаем, что поиск по неизвестному имени вернет false
+    EXPECT_FALSE(table.lookup("not_exist", info));
 }
 
-TEST(PostfixTest, EvaluateComplex) {
-    PostfixEvaluator evaluator;
-    EXPECT_EQ(evaluator.evaluate("2 3 4 * +"), 14);
+// Тест: Обновление значения существующего символа
+TEST(SymbolTableTest, UpdateSymbol) {
+    SymbolTable table;
+
+    // Вставляем переменную
+    table.insert("a", SymbolType::Variable, 1);
+
+    // Обновляем значение с 1 на 2
+    EXPECT_TRUE(table.update("a", 2));
+
+    SymbolInfo info;
+
+    // Убеждаемся, что обновление сработало
+    EXPECT_TRUE(table.lookup("a", info));
+    EXPECT_EQ(info.value, 2);
 }
 
-TEST(PostfixTest, ThrowsOnInvalid) {
-    PostfixEvaluator evaluator;
-    EXPECT_THROW(evaluator.evaluate("2 +"), std::runtime_error);
+// Тест: Попытка обновить несуществующий символ
+TEST(SymbolTableTest, UpdateNotExist) {
+    SymbolTable table;
+
+    // Ожидаем, что update не сработает (символа нет)
+    EXPECT_FALSE(table.update("not_exist", 42));
 }
